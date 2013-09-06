e2e08001c5ea3ff6a1a006492add8c0ba0868403
Sulaeman <me@sulaeman.com>
2013-09-06 21:08:57 +0700

    first commit

---
+++ src/Sule/Tddg/TddgServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+namespace Sule\Tddg;
+
+/*
+ * This file is part of the Template Data Definition Generator
+ *
+ * Author: Sulaeman <me@sulaeman.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Support\ServiceProvider;
+
+class TddgServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('sule/tddg');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		
+	}
+
+	/**
+     * Register the artisan commands.
+     *
+     * @return void
+     */
+    protected function registerCommands()
+    {
+        
+    }
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('tddg');
+	}
+
+}
